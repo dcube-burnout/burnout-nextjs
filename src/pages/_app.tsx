ad922e26fe5d390286cd9b75adc58c323a7f773f
Jun Jie <choo_jun_jie@tech.gov.sg>
2023-03-09 21:28:49 +0800

    [JJ] Solve hydration error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,29 @@
 import '../../src/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import dynamic from 'next/dynamic';
 
 const darkTheme = createTheme({
-  palette: {
-    primary: {
-      // Purple and green play nicely together.
-      main: '#ff6624',
-    },
-    secondary: {
-      // This is green.A700 as hex.
-      main: '#11cb5f',
-    },
-    mode: 'dark',
-  },
+	palette: {
+		primary: {
+			// Purple and green play nicely together.
+			main: '#ff6624',
+		},
+		secondary: {
+			// This is green.A700 as hex.
+			main: '#11cb5f',
+		},
+		mode: 'dark',
+	},
 });
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />{' '}
-    </ThemeProvider>
-  );
+function App({ Component, pageProps }: AppProps) {
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Component {...pageProps} />{' '}
+		</ThemeProvider>
+	);
 }
+
+export default dynamic(() => Promise.resolve(App), { ssr: false });
