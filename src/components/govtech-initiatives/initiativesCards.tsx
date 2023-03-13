a3d8e8ebdde5e1063670b25c876029c1b9671776
ningyuuu <ningyu321@gmail.com>
2023-03-13 17:47:32 +0800

    update initiatives section

--- src/components/govtech-initiatives/initiativesCards.tsx
+++ src/components/govtech-initiatives/initiativesCards.tsx
@@ -2,80 +2,53 @@ import React from 'react';
 import styled from 'styled-components';
 import SectionHeader from '../SectionHeader';
 import Link from 'next/link';
+import { Grid, Typography } from '@mui/material';
+import { MdOutlineEventNote } from 'react-icons/md';
 import Section from '../Section';
-import { MdStarRate } from 'react-icons/md';
 
 export const InitiativesCards = () => {
   return (
     <Section>
-      <SectionHeader headerType="intitatives" title="GovTech's Intitatives" Icon={MdStarRate} />
-      <Grid>
-        <Item>
+      <SectionHeader headerType="intitatives" title="GovTech's Intitatives" Icon={MdOutlineEventNote} />
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
           <Link href="https://supportgowhere.life.gov.sg/categories/mental-health?activeTab=services" target="_blank">
             <Img src={'/img/SupportGoWhere.png'} alt="SupportGoWhere" />
-            <Title>SupportGoWhere</Title>
+            <Typography variant="h6">SupportGoWhere</Typography>
             <br />
-            <Description>Find support schemes and services all in one place.</Description>
+            <Typography>Find support schemes and services all in one place.</Typography>
           </Link>
-        </Item>
+        </Grid>
 
-        <Item>
+        <Grid item xs={4}>
           <Link href="https://www.facebook.com/InsideGovTech/" target="_blank">
             <Img src={'/img/Play@Govtech.png'} alt="Play@Govtech" />
-            <Title>Play@Govtech</Title>
+            <Typography variant="h6">Play@Govtech</Typography>
             <br />
-            <Description>
+            <Typography>
               Join interest groups and hang out with other fellow Govtechies to take your mind off work!
-            </Description>
+            </Typography>
           </Link>
-        </Item>
+        </Grid>
 
-        <Item>
+        <Grid item xs={4}>
           <Link href="" target="_blank">
             <Img src={'/img/Wellness@govtech.png'} alt="Wellness@govtech" />
-            <Title>Wellness@Govtech</Title>
+            <Typography variant="h6">Wellness@Govtech</Typography>
             <br />
-            <Description>
+            <Typography>
               Mental health is equally as important as your physical health.
               <br />
               Join our relaxing sessions of meditation through our telegram group!
-            </Description>
+            </Typography>
           </Link>
-        </Item>
+        </Grid>
       </Grid>
     </Section>
   );
 };
-const Title = styled.h3`
-  margin-top: 1rem;
-`;
-
-const Description = styled.div`
-  margin-bottom: 1rem;
-`;
 
 const Img = styled.img`
   min-height: 15%;
   max-width: 100%;
-`;
-
-const Item = styled.div`
-  padding: 1.5rem;
-  z-index: 100;
-  background-color: #112161;
-  border-radius: 1rem;
-`;
-
-const Grid = styled.div`
-  display: grid;
-  padding: 2rem;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 1rem;
-  color: white;
-
-  @media screen and (max-width: 820px) {
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
-  }
 `;
